b26b2416539791c09d900f878781e986b8705aa2
TUSHAR4541 <tusharpandya1234@yahoo.in>
2023-05-02 14:37:03 +0530

    2 files added, 2 deleted

--- src/components/flights/index.tsx
+++ src/components/flights/index.tsx
@@ -1,12 +1,13 @@
-import { useState, useEffect } from 'react';
+import * as Ract, { useState, useEffect } from 'react';
 
 export default function Flights() {
   useEffect(() => {
     fetch('https://api.example.com/data')
       .then(response => response.json())
-      .then(json => setData(json))
+      .then(json => console.log(json))
       .catch(error => console.error(error));
   }, []);
+
   return (
     <>
       <h1>Hello Tushar!</h1>
