95e764d2b8aaf8e71eae68c36ce8850209b5dd95
TUSHAR4541 <tusharpandya1234@yahoo.in>
2023-05-02 14:26:06 +0530

    1 file modified, 3 added, 1 deleted

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import './style.css';
 
+import Flights from './src/components/flights';
+
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Flights></Flights>
     </div>
   );
 }
